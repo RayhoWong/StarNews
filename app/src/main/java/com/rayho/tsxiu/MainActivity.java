c884826d4647f8fccff8d53d38e6ce732c3fdc93
rayho360@gmail.com <83812536kobe>
2018-11-06 15:54:11 +0800

    StatusBar工具类封装完成

--- app/src/main/java/com/rayho/tsxiu/MainActivity.java
+++ app/src/main/java/com/rayho/tsxiu/MainActivity.java
@@ -1,61 +1,53 @@
 package com.rayho.tsxiu;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.TextView;
-
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @BindView(R.id.message)
-//    TextView mTextMessage;
+    @BindView(R.id.message)
+    TextView mTextMessage;
+    @BindView(R.id.toolbar)
+    Toolbar mToolBar;
 
-//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-//
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
-//            return false;
-//        }
-//    };
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-//                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         StatusBarUtil.setStatusBarTranslucent(this);
-//        StatusBarUtil.setStatusBarColor(this,android.R.color.black);
 
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
--- app/src/main/java/com/rayho/tsxiu/StatusBarUtil.java
+++ app/src/main/java/com/rayho/tsxiu/StatusBarUtil.java
@@ -3,14 +3,21 @@ package com.rayho.tsxiu;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
 /**
  * Created by Rayho on 2018/11/5 0005
+ * 状态栏工具类
  */
 public class StatusBarUtil {
 
+    /**
+     * 状态栏透明
+     * @param context
+     */
     public static void setStatusBarTranslucent(Activity context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
             View decorView = context.getWindow().getDecorView();
@@ -24,18 +31,17 @@ public class StatusBarUtil {
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
         }
 
-        /**
-         * 6.0及以上版本可以更改状态栏的字体颜色，如果状态栏的背景色是浅色或者白色，
-         * 那么字体图标的颜色趋向于深色或者是黑色
-         * 记得根布局设置属性android:fitsSystemWindows="true"，不然toolbar会占用状态栏
-         * @param context
-         */
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             context.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
 
+    /**
+     * 改变状态栏颜色
+     * @param context
+     * @param color
+     */
     public static void setStatusBarColor(Activity context,int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
             View decorView = context.getWindow().getDecorView();
@@ -43,7 +49,7 @@ public class StatusBarUtil {
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
             //状态栏单独设置颜色
-            context.getWindow().setStatusBarColor(color);
+            context.getWindow().setStatusBarColor(context.getResources().getColor(color));
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
             WindowManager.LayoutParams localLayoutParams = context.getWindow().getAttributes();
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
@@ -54,4 +60,76 @@ public class StatusBarUtil {
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
     }
+
+    /**
+     * 隐藏状态栏
+     * @param context
+     */
+    public static void hideStatusBar(Activity context){
+        View decorView = context.getWindow().getDecorView();
+        int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(option);
+    }
+
+    /**
+     * 改变状态栏字体的颜色
+     * 6.0及以上版本可以更改状态栏的字体颜色，如果状态栏的背景色是浅色或者白色，
+     * 那么字体图标的颜色趋向于深色或者是黑色
+     * 记得根布局设置属性android:fitsSystemWindows="true"，不然toolbar会占用状态栏
+     * @param context
+     */
+    public static void changeStatusBarTextColor(Activity context){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            context.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+
+
+    /**
+     * 为ToolBar添加一个状态栏高度的paddingTop
+     * 效果等于fitsSystemWindows=true这个属性
+     * @param context
+     * @param toolbar
+     */
+    public static void setToolBarPaddingTop(Activity context,View toolbar) {
+
+        final float scale = context.getResources().getDisplayMetrics().density;
+
+        int statusBarHeight = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        Log.d("CompatToolbar", "状态栏高度：" + (statusBarHeight/scale) + "dp");
+
+        int compatPadingTop = 0;
+        // android 4.4以上将Toolbar添加状态栏高度的上边距，沉浸到状态栏下方
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            compatPadingTop = statusBarHeight;
+        }
+        toolbar.setPadding(toolbar.getPaddingLeft(), toolbar.getPaddingTop() + compatPadingTop,
+                toolbar.getPaddingRight(), toolbar.getPaddingBottom());
+    }
+
+   /* public int getStatusBarHeight() {
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        Log.d("CompatToolbar", "状态栏高度：" + px2dp(statusBarHeight) + "dp");
+        return statusBarHeight;
+    }
+
+    public float px2dp(float pxVal) {
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (pxVal / scale);
+    }
+*/
+
+
+
+
+
 }
